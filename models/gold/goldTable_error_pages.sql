f3e426ba3e6f116a8cafe4cf1a9fcbf73fe87228
Liam Thireau <liamthireau@MacBook-Air-de-Liam.local>
2026-01-15 12:24:42 +0100

    maj script dbt

--- models/gold/goldTable_error_pages.sql
+++ models/gold/goldTable_error_pages.sql
@@ -5,14 +5,10 @@
 }}
 
 SELECT
-    page_category,
     url AS page,
-    status_code,
-    status_category,
     COUNT(*) AS nb_errors,
     COUNT(DISTINCT ip) AS nb_users_impacted
 FROM {{ ref('silvertable') }}
-WHERE is_error = TRUE
-  AND is_bot = FALSE
-GROUP BY page_category, url, status_code, status_category
+WHERE status_code >= 400
+GROUP BY url
 ORDER BY nb_errors DESC
